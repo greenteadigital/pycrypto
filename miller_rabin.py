0787d02969204617bec380cf6c54ef56797ec81f
Benjamin Hall <w1n.w1th.b3n@gmail.com>
2016-08-06 22:50:12 -0400

    Revert "Revert "Got all headers wired in and Windows calls commented out""
    This reverts commit 4d1886a2056f986e447850dd8876cd3ee6681194.

--- miller_rabin.py
+++ miller_rabin.py
@@ -1,6 +1,5 @@
 
-def isPrime(testing):
-	def isSpsp(testing, known_prime):
+def isSpsp(testing, known_prime):
 		d = testing - 1
 		s = 0
 		while d % 2 == 0:
@@ -15,6 +14,8 @@ def isPrime(testing):
 			t = (t * t) % testing
 			s = s - 1
 		return False
+
+def isPrime(testing):
 	known_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
 		 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 	if testing in known_primes:
