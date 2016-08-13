78b7e60368068cd099181e1d1259f23c5489d44c
Benjamin Hall <w1n.w1th.b3n@gmail.com>
2016-08-13 17:00:17 -0400

    Fix main thread join bug, add hashing for random seed.

--- primes_posix.c
+++ primes_posix.c
@@ -3,24 +3,45 @@
 #include <string.h>
 #include <gmp.h>
 #include <pthread.h>
+#include <sph_md5.h>
 
-const int NTHREADS = 2;
-void *returnedPrime;
-pthread_mutex_t mutex;
+int NTHREADS = 4;
 
-void getRandom(unsigned char *buffer, long numbytes) {
-	// read from /dev/urandom
-	FILE *randfile = fopen("/dev/urandom", "r");
-	int numread = 0;
-	while (numread < numbytes) {
-		numread += fread(buffer, 1, numbytes, randfile);
+unsigned char rand_base[16];
+char rand_base_set = 0;
+
+pthread_mutex_t printlock;
+pthread_mutex_t randbase_lock;
+
+void md5(sph_md5_context *ctxt_ptr, unsigned char *inbuff, const char inbuffsz, unsigned char *outbuff) {
+	
+	sph_md5(ctxt_ptr, inbuff, inbuffsz);
+	sph_md5_close(ctxt_ptr, outbuff);
+}
+
+void getRandom(unsigned char *buffer, const char numbytes, sph_md5_context *ctxt) {
+	
+	if (!rand_base_set) {
+		FILE *randfile = fopen("/dev/urandom", "r");
+		int numread = 0;
+		while (numread < numbytes) {
+			numread += fread((void *) buffer, 1, numbytes, randfile);
+		}
+		pthread_mutex_lock(&randbase_lock);
+		rand_base_set = 1;
+		memcpy(rand_base, buffer, numbytes);
+		pthread_mutex_unlock(&randbase_lock);
+	} else {
+		md5(ctxt, &rand_base[0], numbytes, buffer);
+		pthread_mutex_lock(&randbase_lock);
+		memcpy(rand_base, buffer, numbytes);
+		pthread_mutex_unlock(&randbase_lock);
 	}
 }
 
 void tryPrime(void *bitsize) {
 
-	const int SEED_SZ_BYTES = 16;	// 128 bits
-	const int WORD_SZ_BYTES = __WORDSIZE / 8;
+	const char SEED_SZ_BYTES = 16;	// 128 bits for random seed
 
 	short PRIME_SZ = *((short*)bitsize);
 	unsigned char seed_buff[SEED_SZ_BYTES];
@@ -29,51 +50,56 @@ void tryPrime(void *bitsize) {
 	gmp_randstate_t state;
 	gmp_randinit_mt(state);
 
-	mpz_t seed, randnum;
+	mpz_t seed, randnum, one;
 	mpz_init(seed);
 	mpz_init(randnum);
-
-	getRandom(seed_buff_p, SEED_SZ_BYTES);
-	mpz_import(seed, (SEED_SZ_BYTES/WORD_SZ_BYTES), -1, WORD_SZ_BYTES, 0, 0, seed_buff_p);
+	mpz_init(one);
+	
+	unsigned char single = 1;
+	mpz_import(one, 1, -1, 1, 0, 0, &single);
+	
+	sph_md5_context ctxt;
+	sph_md5_init((void *) &ctxt);
+	
+	getRandom(seed_buff_p, SEED_SZ_BYTES, &ctxt);
+	mpz_import(seed, SEED_SZ_BYTES, -1, 1, 0, 0, seed_buff_p);
 	gmp_randseed(state, seed);
 
 	while(1) {
 		mpz_urandomb(randnum, state, PRIME_SZ);
+		mpz_ior(randnum, randnum, one);	// make sure randnum is odd by bitwise-ORing with 1
 		if (mpz_probab_prime_p(randnum, 17)) break;
 	}
 	
-	pthread_mutex_lock(&mutex);
-	returnedPrime = malloc((PRIME_SZ / 8));
-	mpz_export(returnedPrime, NULL, -1, WORD_SZ_BYTES, 0, 0, randnum);
-	//pthread_mutex_unlock(&mutex);
-
+	pthread_mutex_lock(&printlock);
 	gmp_printf("%Zd", randnum);
-
-	//gmp_randclear(state);
-	//mpz_clear(seed);
-	//mpz_clear(randnum);
-
 	exit(0);
 }
 
-void *getPrime(unsigned short bitlen) {
+void getPrime(unsigned short bitlen) {
 
-	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_init(&printlock, NULL);
+	pthread_mutex_init(&randbase_lock, NULL);
+	pthread_t threads[NTHREADS];
 	
 	for (int i = 0; i != NTHREADS; i++) {
 		pthread_t new_thread;
 		pthread_create(&new_thread, NULL, (void *) &tryPrime, (void *) &bitlen);
-		int join_result;
-		pthread_join(new_thread, (void *) &join_result);
-		
+		threads[i] = new_thread;
 	}
-
-	printf("\nprime pointer points to address %p\n", returnedPrime);
-	return returnedPrime;
+	
+	for (int i = 0; i != sizeof(threads); i++) {
+		pthread_join(threads[i], NULL);
+	}
+	
 }
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) return 1;
-	getPrime(atoi(argv[1]));
+	if (argc == 2) getPrime(atoi(argv[1]));
+	if (argc == 3) {
+		NTHREADS = atoi(argv[2]);
+		getPrime(atoi(argv[1]));
+	}
 	return 0;
 }
