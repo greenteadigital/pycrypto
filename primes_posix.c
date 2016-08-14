d6f4a0260f1536df2b1617ae9a102f1ea79db889
Benjamin Hall <w1n.w1th.b3n@gmail.com>
2016-08-13 22:35:51 -0400

    Post-recovery from "Illegal instruction: 4" desperation commit! ;)

--- primes_posix.c
+++ primes_posix.c
@@ -4,11 +4,12 @@
 #include <gmp.h>
 #include <pthread.h>
 #include <sph_md5.h>
+#include <unistd.h>
 
 int NTHREADS = 4;
 
 unsigned char rand_base[16];
-char rand_base_set = 0;
+unsigned char hash_random = 1;
 
 pthread_mutex_t printlock;
 pthread_mutex_t randbase_lock;
@@ -19,37 +20,34 @@ void md5(sph_md5_context *ctxt_ptr, unsigned char *inbuff, const char inbuffsz, 
 	sph_md5_close(ctxt_ptr, outbuff);
 }
 
-void getRandom(unsigned char *buffer, const char numbytes, sph_md5_context *ctxt) {
+void getRandomHash(unsigned char *buffer, const char numbytes, sph_md5_context *ctxt) {
 	
-	if (!rand_base_set) {
-		FILE *randfile = fopen("/dev/urandom", "r");
-		int numread = 0;
-		while (numread < numbytes) {
-			numread += fread((void *) buffer, 1, numbytes, randfile);
-		}
-		pthread_mutex_lock(&randbase_lock);
-		rand_base_set = 1;
-		memcpy(rand_base, buffer, numbytes);
-		pthread_mutex_unlock(&randbase_lock);
-	} else {
-		md5(ctxt, &rand_base[0], numbytes, buffer);
-		pthread_mutex_lock(&randbase_lock);
-		memcpy(rand_base, buffer, numbytes);
-		pthread_mutex_unlock(&randbase_lock);
+	md5(ctxt, &rand_base[0], numbytes, buffer);
+	pthread_mutex_lock(&randbase_lock);
+	memcpy(rand_base, buffer, numbytes);
+	pthread_mutex_unlock(&randbase_lock);
+}
+
+void getUrandom(unsigned char *buff, unsigned char buffsz) {
+	
+	FILE *randfile = fopen("/dev/urandom", "r");
+	int numread = 0;
+	while (numread < buffsz) {
+		numread += fread((void *) buff, 1, buffsz - numread, randfile);
 	}
 }
 
 void tryPrime(void *bitsize) {
-
+	
 	const char SEED_SZ_BYTES = 16;	// 128 bits for random seed
-
+	
 	short PRIME_SZ = *((short*)bitsize);
 	unsigned char seed_buff[SEED_SZ_BYTES];
 	unsigned char *seed_buff_p = &seed_buff[0];
-
+	
 	gmp_randstate_t state;
 	gmp_randinit_mt(state);
-
+	
 	mpz_t seed, randnum, one;
 	mpz_init(seed);
 	mpz_init(randnum);
@@ -61,10 +59,15 @@ void tryPrime(void *bitsize) {
 	sph_md5_context ctxt;
 	sph_md5_init((void *) &ctxt);
 	
-	getRandom(seed_buff_p, SEED_SZ_BYTES, &ctxt);
+	if (hash_random) {
+		getRandomHash(seed_buff_p, SEED_SZ_BYTES, &ctxt);
+	} else {
+		getUrandom(seed_buff_p, sizeof(rand_base));
+	}
+	
 	mpz_import(seed, SEED_SZ_BYTES, -1, 1, 0, 0, seed_buff_p);
 	gmp_randseed(state, seed);
-
+	
 	while(1) {
 		mpz_urandomb(randnum, state, PRIME_SZ);
 		mpz_ior(randnum, randnum, one);	// make sure randnum is odd by bitwise-ORing with 1
@@ -77,10 +80,12 @@ void tryPrime(void *bitsize) {
 }
 
 void getPrime(unsigned short bitlen) {
-
+	
 	pthread_mutex_init(&printlock, NULL);
 	pthread_mutex_init(&randbase_lock, NULL);
 	pthread_t threads[NTHREADS];
+	
+	getUrandom(&rand_base[0], sizeof(rand_base));
 	
 	for (int i = 0; i != NTHREADS; i++) {
 		pthread_t new_thread;
@@ -96,10 +101,32 @@ void getPrime(unsigned short bitlen) {
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) return 1;
-	if (argc == 2) getPrime(atoi(argv[1]));
-	if (argc == 3) {
-		NTHREADS = atoi(argv[2]);
-		getPrime(atoi(argv[1]));
+	
+	int c;
+	extern char *optarg;
+	extern int optind, optopt, opterr;
+	unsigned short bitlen = 0;
+	
+	while ((c = getopt(argc, argv, "b:t:hu")) != -1) {
+		switch(c) {
+			case 'b':
+				bitlen = atoi(optarg);
+				break;
+			case 't':
+				NTHREADS = atoi(optarg);
+				break;
+			case 'h':
+				hash_random = 1;
+				break;
+			case 'u':
+				hash_random = 0;
+				break;
+			case '?':
+				printf("unknown arg %c\n", optopt);
+				break;
+		}
 	}
+	
+	getPrime(bitlen);
 	return 0;
 }
