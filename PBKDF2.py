1adcf7834fdcba9d1620d1b84a2c631c9a5631d9
Benjamin Hall <w1n.w1th.b3n@gmail.com>
2017-12-20 17:53:51 -0500

    Add userinput module, allow user to pick params for PBKDF2...
    and add compression support.

--- PBKDF2.py
+++ PBKDF2.py
@@ -3,10 +3,10 @@ from hashlib import sha512, sha384, sha256, sha224
 import os
 import struct
 import sys
-
-# # Global constants
-SELECTED_ALGO = 3  # TODO: let user select algo
-EXP_INCR = 1; assert(0 <= EXP_INCR <= 4)  # enforce min/max
+import zlib
+import bz2
+from cStringIO import StringIO as strio
+import userinput as usr
 
 ALGOS = {
 	0: sha224,
@@ -20,26 +20,37 @@ DIGESTSIZES = {
 	2: ALGOS[2]().digest_size,
 	3: ALGOS[3]().digest_size
 }
-DIGESTSZ = DIGESTSIZES[SELECTED_ALGO] 
+COMPRESSION = {
+	0: None,
+	1: zlib,
+	2: bz2,
+}
 CRYPT_EXT = '.phse'
 MAGIC = "PBKDF2-HMAC-SHA2"
-# TODO: allow user input
 PWD_HASH_MULT = 20
+sha2 = None	# set later
 
-# # Global variable, may change when decrypting file encrypted with other than selected algo.
-sha2 = ALGOS[SELECTED_ALGO]
+def _exit():
+	raw_input("\npress Enter to exit...")
+	sys.exit()
 
-def bitPack(algonum, exp_incr):
-	bitstr = bin(algonum)[2:].zfill(4) + bin(exp_incr)[2:].zfill(4)
-
-	return int('0b' + bitstr, 2)
+def bitPack(algonum, exp_incr, compressornum):
+	bitstr = (bin(algonum)[2:].zfill(2)
+			+ bin(exp_incr)[2:].zfill(2)
+			+ bin(compressornum)[2:].zfill(2)
+			+ '00')
+	
+	r = int('0b' + bitstr, 2)
+	return r
 
 def bitUnpack(_int):
 	bitstr = bin(_int)[2:].zfill(8)
-	algonum = int('0b' + bitstr[:4], 2)
-	increment_by = int('0b' + bitstr[4:], 2)
-
-	return (algonum, increment_by)
+	algonum = int('0b' + bitstr[:2], 2)
+	increment_by = int('0b' + bitstr[2:4], 2)
+	compressornum = int('0b' + bitstr[4:6], 2)
+	
+	r = (algonum, increment_by, compressornum)
+	return r 
 
 def constTimeCompare(val1, val2):
 	if len(val1) != len(val2):
@@ -62,27 +73,56 @@ def genKeyBlock(password, salt):
 
 	return sha2(o_pad + sha2(i_pad + salt).digest()).digest()
 
-def getAction(path):
-	print
-	print path
-	print
-
-	return raw_input("(E)ncrypt or (D)ecrypt? ").lower()
-
-# @profile
 def main():
-	global sha2, DIGESTSZ
+	global sha2
 	_input = open(sys.argv[1], 'rb')
-
-	action = getAction(sys.argv[1])
-	while action not in ('e', 'd'):
-		action = getAction(sys.argv[1])
-
+	action = usr.getAction(sys.argv[1])
+		
+	if action in ('i', 'd'):
+		_input = open(sys.argv[1], 'rb')
+		## Verify MAGIC
+		assert(_input.read(len(MAGIC)) == MAGIC) 
+		
+		## Extract key derivation parameters from header
+		algonum, exp_incr, compressornum = bitUnpack(struct.unpack('<B', _input.read(1))[0])
+		_range = xrange(0,4)
+		assert(algonum in _range and exp_incr in _range and compressornum in _range)
+		sha2 = ALGOS[algonum]
+		iter_count = 2 ** (16 + exp_incr)
+		digestsz = DIGESTSIZES[algonum]
+		compressor = COMPRESSION[compressornum]
+		if compressor is not None:
+			compr_name = compressor.__name__
+		else:
+			compr_name = 'none'
+		salt = _input.read(digestsz)
+		embedded_hash = _input.read(digestsz)
+		hashed_password_iters = iter_count * PWD_HASH_MULT
+		
+		if action == 'i':
+			print
+			print "This file was encrypted using the following parameters"
+			print
+			print "hash algorithm:", sha2.__name__
+			print "compression:", compr_name
+			print "key derivation rounds:", iter_count
+			print "password hash rounds:", hashed_password_iters
+			print "salt:", salt.encode('hex')
+			print "hashed password:", embedded_hash.encode('hex')
+			_exit()
+	
 	if action == 'e':
-		iter_count = 2 ** (16 + EXP_INCR)
-		salt = os.urandom(DIGESTSZ)
-		password = getpass.getpass()
-		hdr = MAGIC + struct.pack('<B', bitPack(SELECTED_ALGO, EXP_INCR))
+		algonum = usr.getHashAlgoNum()
+		sha2 = ALGOS[algonum]
+		exp_incr = usr.getExponentIncrement()
+		compressornum = usr.getCompression()
+		compressor = COMPRESSION[compressornum]
+		_input = strio(compressor.compress(_input.read()))
+		iter_count = 2 ** (16 + exp_incr)
+		digestsz = DIGESTSIZES[algonum]
+		salt = os.urandom(digestsz)
+		password = usr.getPassword()
+		hdr = MAGIC + struct.pack('<B', bitPack(algonum, exp_incr, compressornum))
 		hdr += salt
 
 		hashed_pwd = sha2(salt + password).digest()
@@ -91,27 +131,19 @@ def main():
 		hdr += hashed_pwd
 
 	elif action == 'd':
-		_input = open(sys.argv[1], 'rb')
-		
-		# # Verify MAGIC
-		assert(_input.read(len(MAGIC)) == MAGIC) 
-		
-		# # Extract key derivation parameters from header
-		algonum, exp_incr = bitUnpack(struct.unpack('<B', _input.read(1))[0])
-		sha2 = ALGOS[algonum]
-		iter_count = 2 ** (16 + exp_incr)
-		DIGESTSZ = DIGESTSIZES[algonum]
-		salt = _input.read(DIGESTSZ)
-		embedded_hash = _input.read(DIGESTSZ)
-
 		password = getpass.getpass()
+		sys.stdout.write("\nHashing password...")
+		sys.stdout.flush()
 		hashed_pwd = sha2(salt + password).digest()
-		for _ in xrange(iter_count * PWD_HASH_MULT):
+		for _ in xrange(hashed_password_iters):
 			hashed_pwd = sha2(salt + hashed_pwd).digest()
+		sys.stdout.write("\ndone...")
+		sys.stdout.flush()
 		assert(constTimeCompare(embedded_hash, hashed_pwd))
 
 	# # Key stretching
 	sys.stdout.write("\nDeriving key...")
+	sys.stdout.flush()
 	keyblock = genKeyBlock(password, salt)
 	for _ in xrange(iter_count):
 		keyblock = genKeyBlock(keyblock, salt)
@@ -121,27 +153,37 @@ def main():
 		out = open(sys.argv[1] + CRYPT_EXT, 'wb')
 		out.write(hdr)
 		sys.stdout.write("Encrypting...")
+		sys.stdout.flush()
 	
 	elif action == 'd':
 		out = open(sys.argv[1].replace(CRYPT_EXT, ''), 'wb')
 		sys.stdout.write("Decrypting...")
+		sys.stdout.flush()
 	
 	while 1:
-		in_bytes = _input.read(DIGESTSZ)
+		in_bytes = _input.read(digestsz)
 		if in_bytes:
 			keyblock = genKeyBlock(keyblock, salt)
 			outstr = ''
-			for threadnum in xrange(len(in_bytes)):
-				outstr += (chr(ord(keyblock[threadnum]) ^ ord(in_bytes[threadnum])))
+			for bytenum in xrange(len(in_bytes)):
+				outstr += (chr(ord(keyblock[bytenum]) ^ ord(in_bytes[bytenum])))
 			out.write(outstr)
 		else:
 			out.close()
 			break
+		
+	if action == 'd' and compressor is not None:
+		outplain = compressor.decompress(open(sys.argv[1].replace(CRYPT_EXT, ''), 'rb').read())
+		open(sys.argv[1].replace(CRYPT_EXT, ''), 'wb').write(outplain)
+		
 	print "done."
+	_exit()
 
 if __name__ == "__main__" and len(sys.argv) == 2:
-	
-	main()
-
-raw_input("\npress Enter to exit...")
+	try:
+		main()
+	except AssertionError as e:
+		print "Critical assertion failed in function main()!"
+		raise e
+	exit()
 # # EOF
---
+++ userinput.py
@@ -0,0 +1,71 @@
+import getpass
+import sys
+
+def getCompression():
+    num_selected = askCompression()
+    while num_selected not in ('1','2','3'):
+        num_selected = askCompression()
+    return int(num_selected) - 1
+
+def askCompression():
+    return raw_input('''
+Select compression:
+
+(1) None
+(2) zlib
+(3) bz2
+
+Enter 1,2,or 3: ''')
+    
+def getExponentIncrement():
+    num_selected = askIterations()
+    while num_selected not in ('1','2','3','4'):
+        num_selected = askIterations()
+    return int(num_selected) - 1
+
+def askIterations():
+    return raw_input('''
+Select number of iterations:
+
+ #     Key derivation rounds    Password hashing rounds
+---    ---------------------    -----------------------
+(1)                   65,536                  1,310,720
+(2)                  131,072                  2,621,440
+(3)                  262,144                  5,242,880
+(4)                  524,288                 10,485,760
+
+Enter 1,2,3 or 4: ''')
+
+def getHashAlgoNum():
+    num_selected = askHashAlgo()
+    while num_selected not in ('1','2','3','4'):
+        num_selected = askHashAlgo()
+    return int(num_selected) - 1
+    
+def askHashAlgo():
+    return raw_input('''
+Select hash algorithm:
+(1) SHA224
+(2) SHA256
+(3) SHA384
+(4) SHA512
+
+Enter 1,2,3 or 4: ''')
+
+def getAction(path):
+    print
+    print path
+    print
+    action = raw_input("(E)ncrypt, (D)ecrypt, or (I)nfo? ").lower()
+    while action not in ('e', 'd', 'i'):
+        action = getAction(sys.argv[1])
+    return action
+
+def getPassword():
+    password0 = getpass.getpass()
+    password1 = getpass.getpass("Re-enter password: ")
+    if password0 == password1:
+        return password0
+    else:
+        print "Passwords do not match."
+        return getPassword()
